b7fd9a94780217115bba58cad229ff83f704a561
jivajb <96937406+jivajb@users.noreply.github.com>
2025-05-03 16:26:26 -0700

    update text editor page

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,37 @@
-// // app/page.tsx
-// import Header from './components/Header'
-// import Textbox from './components/Textbox'
-
-// export default function HomePage() {
-//   return (
-//     <>
-//       <Header />
-//       <main className="min-h-[calc(100vh-72px)] bg-brand-ivory">
-//         <Textbox />
-//       </main>
-//     </>
-//   )
-// }
-
 "use client";
 import RichTextEditor from "./components/rich-text-editor/RichTextEditor"
-import Header from "./components/Header";
-import GenerateLink from "./components/GenerateLink";
-import { useState } from "react";
+import Header from "./components/Header"
+import GenerateLink from "./components/GenerateLink"
+import { useState } from "react"
+import Sidebar from "./components/Sidebar"
 
+
+const documents = [
+  'Test1',
+  'Test2',
+  'Test3',
+  'Test4',
+  'Test5',
+  'Test6',
+];
 export default function Home() {
-  const [post, setPost] = useState("");
+  const [post, setPost] = useState("")
 
   const onChange = (content: string) => {
-    setPost(content);
-    console.log(content);
-  };
+    setPost(content)
+    console.log(content)
+  }
 
   return (
-    <>
-      <Header />
+    <div>{/* Sidebar */}
+      <Sidebar documents={documents} />
+      
       <div className="max-w-3xl mx-auto py-8">
         <RichTextEditor content={post} onChange={onChange} />
         <GenerateLink content={post} />
       </div>
-    </>
     
-  );
+    </div> /* Sidebar */
+    
+  )
 }
